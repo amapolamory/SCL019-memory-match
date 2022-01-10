c978f3f697ea160b3e264f46814b9d1491ffcaae
amapolamory <amapola.rebeca.mory@gmail.com>
2022-01-10 11:09:16 -0300

    changes-puppies

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
+import puppies from '../data/puppies/puppies.js';
+console.log(puppies);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
