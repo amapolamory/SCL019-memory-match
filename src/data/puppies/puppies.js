f91932cc0415dbbe315a4c64f35a35d8059a5165
amapolamory <amapola.rebeca.mory@gmail.com>
2022-01-10 09:52:46 -0300

    namechanges

--- src/data/puppies/puppies.js
+++ src/data/puppies/puppies.js
@@ -1,12 +1,12 @@
 export default {
     name: 'puppies',
     items: [
-      { id: 'Cocker', image: 'https://www.serebii.net/pokemongo/pokemon/001.png', bgColor: '#339933' },
-      { id: 'Collie', image: 'https://www.serebii.net/pokemongo/pokemon/002.png', bgColor: '#339933' },
-      { id: 'Francés', image: 'https://www.serebii.net/pokemongo/pokemon/003.png', bgColor: '#339933' },
-      { id: 'Inglés', image: 'https://www.serebii.net/pokemongo/pokemon/004.png', bgColor: '#F05032' },
-      { id: 'Terrier', image: 'https://www.serebii.net/pokemongo/pokemon/005.png', bgColor: '#F05032' },
-      { id: 'Hound', image: 'https://www.serebii.net/pokemongo/pokemon/006.png', bgColor: '#F05032' },
+      { id: 'Cocker', image: '../img/Cocker.jpg', bgColor: '#339933' },
+      { id: 'Collie', image: '../img/Collie.jpg', bgColor: '#339933' },
+      { id: 'Francés', image: '../img/Francés.jpg', bgColor: '#339933' },
+      { id: 'Inglés', image: '../img/Inglés.jpg', bgColor: '#F05032' },
+      { id: 'Terrier', image: '../img/Terrier.jpg', bgColor: '#F05032' },
+      { id: 'Hound', image: '../img/Hound.jpg', bgColor: '#F05032' },
      
     ],
   };
