b763e75415139b07821c91cf57d33282b2c4c063
amapolamory <amapola.rebeca.mory@gmail.com>
2022-01-10 10:36:28 -0300

    changes

--- src/data/puppies/puppies.js
+++ src/data/puppies/puppies.js
@@ -1,12 +1,12 @@
 export default {
     name: 'puppies',
     items: [
-      { id: 'Cocker', image: '../img/Cocker.jpg', bgColor: '#339933' },
-      { id: 'Collie', image: '../img/Collie.jpg', bgColor: '#339933' },
-      { id: 'Francés', image: '../img/Francés.jpg', bgColor: '#339933' },
-      { id: 'Inglés', image: '../img/Inglés.jpg', bgColor: '#F05032' },
-      { id: 'Terrier', image: '../img/Terrier.jpg', bgColor: '#F05032' },
-      { id: 'Hound', image: '../img/Hound.jpg', bgColor: '#F05032' },
+      { id: 'Cocker', image: '../../img/Cocker.jpg', bgColor: '#339933' },
+      { id: 'Collie', image: '../../img/Collie.jpg', bgColor: '#339933' },
+      { id: 'Francés', image: '../../img/Francés.jpg', bgColor: '#339933' },
+      { id: 'Inglés', image: '../../img/Inglés.jpg', bgColor: '#F05032' },
+      { id: 'Terrier', image: '../../img/Terrier.jpg', bgColor: '#F05032' },
+      { id: 'Hound', image: '../../img/Hound.jpg', bgColor: '#F05032' },
      
     ],
   };
