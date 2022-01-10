0e5fc6754e9d46782add5c70012f1c808db9b71d
amapolamory <amapola.rebeca.mory@gmail.com>
2022-01-10 09:20:19 -0300

    cambiopuppies

---
+++ src/data/puppies/puppies.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'puppies',
+    items: [
+      { id: 'Cocker', image: 'https://www.serebii.net/pokemongo/pokemon/001.png', bgColor: '#339933' },
+      { id: 'Collie', image: 'https://www.serebii.net/pokemongo/pokemon/002.png', bgColor: '#339933' },
+      { id: 'Francés', image: 'https://www.serebii.net/pokemongo/pokemon/003.png', bgColor: '#339933' },
+      { id: 'Inglés', image: 'https://www.serebii.net/pokemongo/pokemon/004.png', bgColor: '#F05032' },
+      { id: 'Terrier', image: 'https://www.serebii.net/pokemongo/pokemon/005.png', bgColor: '#F05032' },
+      { id: 'Hound', image: 'https://www.serebii.net/pokemongo/pokemon/006.png', bgColor: '#F05032' },
+     
+    ],
+  };
