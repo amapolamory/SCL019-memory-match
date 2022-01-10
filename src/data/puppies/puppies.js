d8cfe704fac1ccbceb65ec3ee52fd79a2861f13a
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-01-10 10:42:44 -0300

    Change carpeta

--- src/data/puppies/puppies.js
+++ src/data/puppies/puppies.js
@@ -1,7 +1,7 @@
 export default {
     name: 'puppies',
     items: [
-      { id: 'Cocker', image: '../img/Cocker.jpg', bgColor: '#339933' },
+      { id: 'Cocker', image: '../../img/Cocker.jpg', bgColor: '#339933' },
       { id: 'Collie', image: '../img/Collie.jpg', bgColor: '#339933' },
       { id: 'Francés', image: '../img/Francés.jpg', bgColor: '#339933' },
       { id: 'Inglés', image: '../img/Inglés.jpg', bgColor: '#F05032' },
