26e8fbae095ec913ae7bfc773287354e9cc94872
amapolamory <amapola.rebeca.mory@gmail.com>
2022-01-11 08:33:49 -0300

    changesmatch

---
+++ src/components/match.js
@@ -0,0 +1,18 @@
+
+const getData= () => [
+
+    { id: 'Cocker', image: '../../img/Cocker.jpg', bgColor: '#339933' },
+    { id: 'Collie', image: '../../img/Collie.jpg', bgColor: '#339933' },
+    { id: 'Francés', image: '../../img/Francés.jpg', bgColor: '#339933' },
+    { id: 'Inglés', image: '../../img/Inglés.jpg', bgColor: '#F05032' },
+    { id: 'Terrier', image: '../../img/Terrier.jpg', bgColor: '#F05032' },
+    { id: 'Hound', image: '../../img/Hound.jpg', bgColor: '#F05032' },
+];
+
+const random = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5)
+ return cardData  ;
+};
+
+random()
