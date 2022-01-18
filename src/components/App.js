0b6989f556b04f6a3211be5963c8d3e678ca0996
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-01-18 10:27:36 -0300

    Comentarios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import puppies from '../data/puppies/puppies.js';
 
-const cards = puppies.items;
-const doubleCards= cards.concat(cards);
+const cards = puppies.items;  
+const doubleCards= cards.concat(cards); //almacenamos las cartas duplicadas (cards+cards)
 console.log(doubleCards);
 
-function shuffle (array){
+//Función para aleatorizar las cartas
+function shuffle (array){  
   for(let i = array.length - 1 ; i > 0 ;i--){
    let mate = Math.floor(Math.random()*(i+1));
    let tempo = array[i];
@@ -12,13 +13,14 @@ function shuffle (array){
    array[mate] = tempo;
    }
   }
+
+  //Declaramos dos arrays: 1)Cartas clickeadas 2)Match
   let clickCard = [];
   let matchedCard = [];
 
-  
-
-
+  //Función para crear estructura y añadir funcionalidad al juego
   const App = () => {
+
     // CONTENEDOR PADRE DEL JUEGO
     const gameContainer= document.createElement('div');
       gameContainer.className= 'gameContainer';    
@@ -26,81 +28,85 @@ function shuffle (array){
       const cardsGrid= document.createElement('div');
       cardsGrid.id = 'cardsGrid';
       cardsGrid.className = 'cardsGrid';
-      gameContainer.appendChild(cardsGrid);
+      gameContainer.appendChild(cardsGrid); //cardsGrid se crea como hijo de gameContainer
       
-      // MUESTRA LAS CARTAS DEL GRID
-      const gridBoard = function()  {
+      // FUNCIÓN QUE MUESTRA LAS CARTAS DEL GRID
+      const gridBoard = function() {
 
         cardsGrid.innerHTML = "";
         // RECORRE LA DATA
         for (let i = 0; i < doubleCards.length; i++ );
 
         // INSERTA IMAGENES EN FRONTFACE Y BACKFACE
-        let boardCard = document.createElement('div');
+         let boardCard = document.createElement('div');
          boardCard.className ='boardCard';
+
          let backFace = document.createElement('img');
          backFace.setAttribute('src' ,'../img/reverso_carta.png');
+
+          //segun lo que entiendo si hace un match se remplaza la imagen del backface 
+          //por la que hizo match de frontface   
          if(doubleCards[i].matched){
-          backFace.setAttribute('src', doubleCards[i].image) //segun lo que entiendo si hace un macth se remplaza la imagen del backface por la que hizo match de frontface   
+          backFace.setAttribute('src', doubleCards[i].image)
         }
-        console.log(backFace)
-       //backface queda en el dentro del frontFace 
+         //backface queda en el dentro del frontFace 
         frontFace.appendChild(backFace);
        
-        backFace.addEventListener('click', function (){
-          // si se a clickeado menos de dos cartas se le da un tiempo 
-          if (clickCard.length < 2){
-            backFace.setAttribute('src', doubleCards[i].image)
+        backFace.addEventListener('click', function (){ //función que permite clickear una carta
+            if (clickCard.length < 2){                  //Sólo se puede clickear 2 cartas cada vez
+            backFace.setAttribute('src', doubleCards[i].image) //Muestra imagen de carta cuando se gira
 
-                 clickCard.push(doubleCards[i]);
-                  setTimeout( () => {
+                 clickCard.push(doubleCards[i]); //Guarda cartas clickeadas en array declarado
+                  setTimeout( () => {            //Define el tiempo del giro de carta
                    flipBack (doubleCards, backFace[i]);
                   },500);
-                
                 }
-                
-     });
-        //Cardsgrid es padre de frontFace lo que hace que se quede dentro de ese espacio
-      cardsGrid.appendChild(frontFace);
+             });
+        //Asignamos el div para cada carta
+        cardsGrid.appendChild(frontFace);
       }
     }
   
-          gridBoard();
-   // tiempo en el que se dan vuelta si no hacen match
+    gridBoard();
+   
+    // tiempo en el que se dan vuelta si no hacen match
       function flipBack(){
         setTimeout(function(){
          matchComplete(clickCard);
        
          },1200);
-          }
+      }
 
-         
+   //Función para todos los match (ganar el juego)      
 function matchComplete(arrayOfClickCard){
   //si las cartas presionadas son exactamente 2 que nos de true y se añadan al array
   if (arrayOfClickCard.length == 2 ){
+    //Si la primera carta es igual a la segunda
     if (arrayOfClickCard[0].id == arrayOfClickCard[1].id){
       arrayOfClickCard[0].matched = true;
       arrayOfClickCard[1].matched = true;
       matchedCard++;
-      // si se completaron los match que se abra el modal
+      // si se completaron los 6 match que se abra el modal
           if (matchedCard === 6){
         window.location()//pagina del modal
    
-        for (let i = 0 ; i < 11 ; i++ ){//corre la data si no ha encontrado todos los match
+        for (let i = 0 ; i <= 11 ; i++ ){//recorre la data si no ha encontrado todos los match
           doubleCards[i].matched = false;
+
          // que se revuelvan las cartas una vez terminado el juego 
           shuffle(doubleCards);
           matchedCard = 0;
         }
       }
+
+      //Reiniciar función para jugar desde 0
       else (gridBoard())
           arrayOfClickCard.length = 0;   
-
   }
         
 
 }
-  return gameContainer
+  return gameContainer //Consultar
      
 };
   
