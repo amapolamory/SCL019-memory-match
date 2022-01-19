42650d8551d1440339d0e97478c4d6738c5fb215
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-01-18 22:30:14 -0300

    Reseteo de código

---
+++ src/components/initGame.js
@@ -0,0 +1,44 @@
+const initGame = () => {
+
+  //Aquí va función para ocultar la página de inicio
+  const startGame = () => { 
+        
+    document.getElementsByClassName('container');
+    container.style.display = 'none';
+    document.getElementById('root').appendChild(gameing()); 
+    }
+    
+
+// container padre
+const container = document.createElement('div');
+  container.className = 'container'; 
+
+//contenedor Título
+const title1 = document.createElement('div');
+title1.className = 'title1';
+container.appendChild(title1); 
+
+
+
+//div hijo que almacena el gif animado
+const contGif = document.createElement('div');
+contGif.className = 'dogSmile';
+container.appendChild(contGif); 
+
+  
+//div hijo de container que almacena el botón start
+     const contStart = document.createElement('div');//   contStart.className = 'start';
+     contStart.className = 'start';
+     contStart.addEventListener('click', startGame); 
+     container.appendChild(contStart); 
+
+//pié de página
+const footer = document.createElement('footer')
+footer.textContent= "© 2022 Laboratoria - Creado por Amapola Mory & Mayorlen Ortega"
+footer.className = 'footer';
+container.appendChild(footer); 
+
+   
+  return container;
+};
+
